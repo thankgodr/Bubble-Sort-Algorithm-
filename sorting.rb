93de16d190403e918d01128225ddf9e7e535c0f7
ThankGod Richard <madueketf@gmail.com>
2020-03-11 15:44:31 +0100

    Indentation coverted to 2 spaces

--- sorting.rb
+++ sorting.rb
@@ -1,27 +1,27 @@
 def bubble_sort(arr)
-    loop do
-        status = false
-        (arr.length-1).times do |i|
-            if arr[i] > arr[i+1]
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-                status = true
-            end
-        end   
-        break if not status
-    end   
-   arr
+  loop do
+    status = false
+    (arr.length-1).times do |i|
+      if arr[i] > arr[i+1]
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        status = true
+      end
+    end
+    break if not status
+  end
+  arr
 end
 
 def bubble_sort_by(arr)
-    loop do
-        status = false
-        (arr.length-1).times do |i|
-            if yield(arr[i], arr[i+1]) > 0
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-                status = true
-            end
-        end   
-        break if not status
-    end   
-   arr
+  loop do
+    status = false
+    (arr.length-1).times do |i|
+      if yield(arr[i], arr[i+1]) > 0
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        status = true
+      end
+    end
+    break if not status
+  end
+  arr
 end
