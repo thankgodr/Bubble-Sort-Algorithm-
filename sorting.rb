4a1ed39d2776d0117d0b60a12e310eb80f4d30e4
ThankGod Richard <madueketf@gmail.com>
2020-03-12 13:51:22 +0100

    Linted code

--- sorting.rb
+++ sorting.rb
@@ -1,13 +1,13 @@
 def bubble_sort(arr)
   loop do
     status = false
-    (arr.length-1).times do |i|
-      if arr[i] > arr[i+1]
-        arr[i], arr[i+1] = arr[i+1], arr[i]
+    (arr.length - 1).times do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
         status = true
       end
     end
-    break if not status
+    break unless status
   end
   arr
 end
@@ -15,13 +15,13 @@ end
 def bubble_sort_by(arr)
   loop do
     status = false
-    (arr.length-1).times do |i|
-      if yield(arr[i], arr[i+1]) > 0
-        arr[i], arr[i+1] = arr[i+1], arr[i]
+    (arr.length - 1).times do |i|
+      if yield(arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
         status = true
       end
     end
-    break if not status
+    break unless status
   end
   arr
 end
